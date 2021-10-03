dcd6e7ca101cfaebd9abc0553017c9a4e0cd4d48
Alon Tzarafi <alontzarafi@gmail.com>
2021-10-03 18:18:38 +0300

    Add function FindComponentMatchStringGlobal()

--- FindUtil.cs
+++ FindUtil.cs
@@ -11,12 +11,17 @@ namespace ExhibitionUtils
                 .GetComponentsInChildren<T>()
                 .FirstOrDefault(comp => comp.name == str);
         }
+
+        static public T FindComponentMatchStringGlobal<T>(string str) where T : Component
+        {
+            return GameObject.FindObjectsOfType<T>()
+                .First(go => go.name == str);
+        }
+
         static public GameObject FindGameObjectMatchName(string str)
-        // איזה יופי
         {
             Debug.Log($"Finding GO with name: {str}");
-            return GameObject.FindObjectsOfType<GameObject>()
-                .First(go => go.name == str);
+            return FindComponentMatchStringGlobal<GameObject>(str);
         }
 
     }
